b6ea6660d2212292b0b277ad4a3f5dc67e158371
AngelGD <angelgabriel.gd@gmail.com>
2024-12-21 22:14:10 -0500

    correciones test

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import saludo from '../src/cli.js';
+import brainGames from '../src/cli.js';
 
-export default () => {
-  console.log('¡Bienvenido a Brain Games!');
-  const nombre = readlineSync.question('¿Cuál es tu nombre? ');
-  console.log(saludo(nombre));
-  return nombre;
-};
+export default brainGames;
--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,8 @@
-export default (str) => `!Hola, ${str}¡`;
+import readlineSync from 'readline-sync';
+
+export default () => {
+  console.log('¡Bienvenido a Brain Games!');
+  const nombre = readlineSync.question('¿Cuál es tu nombre? ');
+  console.log(`!Hola, ${nombre}¡`);
+  return nombre;
+};
