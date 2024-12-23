a8d6cd381082ee5ce47b781639efe7aef18511a0
AngelGD <angelgabriel.gd@gmail.com>
2024-12-22 22:22:13 -0500

    correciones test

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('¡Bienvenido a Brain Games!');
   const nombre = readlineSync.question('¿Cuál es tu nombre? ');
-  console.log(`!Hola, ${nombre}¡`);
+  console.log(`¡Hola, ${nombre}!`);
   return nombre;
 };
